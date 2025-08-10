b323a8892a874f8b9cd660caf273f1b6787277f3
Luca Wang <contact@lucwang.me>
2025-08-10 00:58:46 -0400

    removed unnecessary code

--- frontend/src/app/(pushup)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type React from "react"
-import Providers from "../providers"
-
-export default function PushupLayout({ children }: { children: React.ReactNode }) {
-  return <Providers>{children}</Providers>
-}
